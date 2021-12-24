82ef4c3032369fae67ae4e3bddf224794165627c
xuexia <xuexia.li@mobvista.com>
2021-12-24 20:30:23 +0800

    fix

--- internal/chttp/http_request.go
+++ internal/chttp/http_request.go
@@ -7,7 +7,30 @@ import (
     "net/http"
 )
 
-func HttpGet(url,token string) (resp_body map[string]interface{}, err error) {
+type ItemInfo struct {
+    Key                         string     `toml:"key"`
+    Value                       string     `toml:"value"`
+    DataChangeCreatedBy         string     `toml:"dataChangeCreatedBy"`
+    DataChangeLastModifiedBy    string     `toml:"dataChangeLastModifiedBy"`
+    DataChangeCreatedTime       string     `toml:"dataChangeCreatedTime"`
+    DataChangeLastModifiedTime  string     `toml:"dataChangeLastModifiedTime"`
+}
+
+type NamespaceInfo struct {
+    AppId                       string     `toml:"appId"`
+    ClusterName                 string     `toml:"clusterName"`
+    NamespaceName               string     `toml:"namespaceName"`
+    Comment                     string     `toml:"comment"`
+    Format                      string     `toml:"format"`
+    IsPublic                    string     `toml:"isPublic"`
+    DataChangeCreatedBy         string     `toml:"dataChangeCreatedBy"`
+    DataChangeLastModifiedBy    string     `toml:"dataChangeLastModifiedBy"`
+    DataChangeCreatedTime       string     `toml:"dataChangeCreatedTime"`
+    DataChangeLastModifiedTime  string     `toml:"dataChangeLastModifiedTime"`
+    Items                       *ItemInfo  `toml:"items"`
+}
+
+func HttpGet(url,token string) (resp_body *NamespaceInfo, err error) {
     client := &http.Client{}
     req,_ := http.NewRequest("GET",url,nil)
     req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
@@ -21,7 +44,7 @@ func HttpGet(url,token string) (resp_body map[string]interface{}, err error) {
     return
 }
 
-func HttpPostForm(url, token string, data map[string]interface{})(resp_body map[string]interface{}, err error) {
+func HttpPostForm(url, token string, data map[string]interface{})(resp_body *NamespaceInfo, err error) {
     client := &http.Client{}
     bytesData, _ := json.Marshal(data)
     req, _ := http.NewRequest("POST",url,bytes.NewReader(bytesData))
