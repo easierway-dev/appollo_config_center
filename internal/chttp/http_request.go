d84c86d6faf299804f98ddafc79cc87bb2eb57d4
xuexia <xuexia.li@mobvista.com>
2021-12-24 14:58:18 +0800

    fix

--- internal/chttp/http_request.go
+++ internal/chttp/http_request.go
@@ -13,11 +13,12 @@ func httpGet(url,token string) (resp_body string, err error) {
     req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
     req.Header.Set("Authorization",token)
     resp,_ := client.Do(req)
-    resp_body, err = ioutil.ReadAll(resp.Body)
+    body, err := ioutil.ReadAll(resp.Body)
     if err != nil {
         return "{}", err
     }
-    return string(resp_body), nil
+    resp_body = string(body)
+    return resp_body, nil
 }
 
 func httpPostForm(url string, token, data map[string]interface{}) {
@@ -31,5 +32,6 @@ func httpPostForm(url string, token, data map[string]interface{}) {
     if err != nil {
         return "{}", err
     }
+    resp_body = string(body)
     return string(resp_body), nil
 }
