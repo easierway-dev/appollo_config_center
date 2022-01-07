27b62b634d4cfed4d968d517f118ca539c7469a0
xuexia <xuexia.li@mobvista.com>
2022-01-07 12:16:54 +0800

    fix

--- internal/chttp/http_request.go
+++ internal/chttp/http_request.go
@@ -7,25 +7,23 @@ import (
     "net/http"
 )
 
-func HttpGet(url,token string) (resp_body string, err error) {
+func HttpGet(url,token string) (resp_body interface{}, err error) {
     client := &http.Client{}
     req,_ := http.NewRequest("GET",url,nil)
     req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
     req.Header.Set("Authorization",token)
     resp,_ := client.Do(req)
-    body, err := ioutil.ReadAll(resp.Body)
-    resp_body = string(body)
+    resp_body, err := ioutil.ReadAll(resp.Body)
     return 
 }
 
-func HttpPostForm(url, token string, data map[string]interface{})(resp_body string, err error) {
+func HttpPostForm(url, token string, data map[string]interface{})(resp_body interface{}, err error) {
     client := &http.Client{}
     bytesData, _ := json.Marshal(data)
     req, _ := http.NewRequest("POST",url,bytes.NewReader(bytesData))
     req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
     req.Header.Set("Authorization",token)
     resp, _ := client.Do(req)
-    body, err := ioutil.ReadAll(resp.Body)
-    resp_body = string(body)
+    resp_body, err := ioutil.ReadAll(resp.Body)
     return 
 }
