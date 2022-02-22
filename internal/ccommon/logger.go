e28637af8de9fdcf0063c71c1147a96d9531e9f2
jilong.jian <jilong.jian@mobvista.com>
2022-02-22 14:59:15 +0800

    fix8

--- internal/ccommon/logger.go
+++ internal/ccommon/logger.go
@@ -1,8 +1,8 @@
 package ccommon
 
 import (
-	"fmt"
 	"errors"
+	"fmt"
 
 	"gitlab.mobvista.com/mvbjqa/appollo_config_center/internal/cnotify"
 	"gitlab.mobvista.com/voyager/zlog"
@@ -11,8 +11,8 @@ import (
 var CLogger *ccLogger
 
 const (
-	DefaultDingType = ""
-	InitDingType = "init"
+	DefaultDingType     = ""
+	InitDingType        = "init"
 	DefaultPollDingType = "poll"
 )
 
@@ -36,8 +36,8 @@ func NewconfigCenterLogger(logCfg *LogCfg) (*ccLogger, error) {
 	return logger, nil
 }
 
-func GetDingInfo(appid string, itype string) (dingKeys []string,dingusers []string, userMap map[string]string, isAtall bool) {
-	if appid == "" && itype == "info"{
+func GetDingInfo(appid string, itype string) (dingKeys []string, dingusers []string, userMap map[string]string, isAtall bool) {
+	if appid == "" && itype == "info" {
 		return
 	}
 	//local config
@@ -47,99 +47,102 @@ func GetDingInfo(appid string, itype string) (dingKeys []string,dingusers []stri
 	dingusers = AppConfiger.DingUsers
 	userMap = AppConfiger.DingUserMap
 	isAtallTmp := AppConfiger.IsAtAll
-	for _,v := range dingKeys{
-		fmt.Println("dingKey为:"+v + "\n")
+	for _, v := range dingKeys {
+		fmt.Println("dingKey为:" + v + "\n")
 	}
-	for _,v := range dingusers{
-		fmt.Println("dingusers为:"+v + "\n")
+	for _, v := range dingusers {
+		fmt.Println("dingusers为:" + v + "\n")
 	}
-	for k,v := range userMap{
-		fmt.Println("userMapKey为:"+k+" "+"userMapValue为"+v+"\n")
+	for k, v := range userMap {
+		fmt.Println("userMapKey为:" + k + " " + "userMapValue为" + v + "\n")
 	}
-	fmt.Println("isAtAllTmp为:"+string(isAtallTmp))
+	fmt.Println("isAtAllTmp为:" + string(isAtallTmp))
 	//uniq appid config
 	if AppConfiger.AppConfigMap != nil {
-		if _,ok := AppConfiger.AppConfigMap[appid];ok {
-			if len(AppConfiger.AppConfigMap[appid].DingKeys) > 0 {
-				dingKeys = AppConfiger.AppConfigMap[appid].DingKeys
-			}
-			if len(AppConfiger.AppConfigMap[appid].DingUsers) > 0{
-				dingusers = AppConfiger.AppConfigMap[appid].DingUsers
-			}
-			for key,value := range AppConfiger.AppConfigMap[appid].DingUserMap {
-				if userMap == nil {
-					userMap = map[string]string{}
-				}
-				userMap[key] = value
-			}
-			if AppConfiger.AppConfigMap[appid].IsAtAll != 0 {
-				isAtallTmp = AppConfiger.AppConfigMap[appid].IsAtAll
-			}
+		if _, ok := AppConfiger.AppConfigMap[appid]; ok {
+			//if len(AppConfiger.AppConfigMap[appid].DingKeys) > 0 {
+			//	dingKeys = AppConfiger.AppConfigMap[appid].DingKeys
+			//}
+			//if len(AppConfiger.AppConfigMap[appid].DingUsers) > 0 {
+			//	dingusers = AppConfiger.AppConfigMap[appid].DingUsers
+			//}
+			//for key, value := range AppConfiger.AppConfigMap[appid].DingUserMap {
+			//	if userMap == nil {
+			//		userMap = map[string]string{}
+			//	}
+			//	userMap[key] = value
+			//}
+			//if AppConfiger.AppConfigMap[appid].IsAtAll != 0 {
+			//	isAtallTmp = AppConfiger.AppConfigMap[appid].IsAtAll
+			//}
+			InitAppConfigMap(AppConfiger.AppConfigMap,appid,isAtallTmp)
 		}
 	}
-	for _,v := range dingKeys{
-		fmt.Println("dingKey为:"+v + "\n")
+	for _, v := range dingKeys {
+		fmt.Println("dingKey为:" + v + "\n")
 	}
-	for _,v := range dingusers{
-		fmt.Println("dingusers为:"+v + "\n")
+	for _, v := range dingusers {
+		fmt.Println("dingusers为:" + v + "\n")
 	}
-	for k,v := range userMap{
-		fmt.Println("userMapKey为:"+k+" "+"userMapValue为"+v+"\n")
+	for k, v := range userMap {
+		fmt.Println("userMapKey为:" + k + " " + "userMapValue为" + v + "\n")
 	}
-	fmt.Println("isAtAllTmp为:"+string(isAtallTmp))
+	fmt.Println("isAtAllTmp为:" + string(isAtallTmp))
 	//apollo global_config
 	if DyAgolloConfiger != nil {
-		if dyAgoCfg,ok := DyAgolloConfiger[namespace];ok {
+		if dyAgoCfg, ok := DyAgolloConfiger[namespace]; ok {
 			//default config
 			if dyAgoCfg.AppConfig != nil {
-				if len(dyAgoCfg.AppConfig.DingKeys) > 0 {
-					dingKeys = dyAgoCfg.AppConfig.DingKeys
-				}
-				if len(dyAgoCfg.AppConfig.DingUsers) > 0{
-					dingusers = dyAgoCfg.AppConfig.AppConfigMap[appid].DingUsers
-				}
-				for key,value := range dyAgoCfg.AppConfig.DingUserMap {
-					if userMap == nil {
-						userMap = map[string]string{}
-					}
-					userMap[key] = value
-				}
-				if dyAgoCfg.AppConfig.IsAtAll != 0 {
-					isAtallTmp = dyAgoCfg.AppConfig.IsAtAll
-				}
+				//if len(dyAgoCfg.AppConfig.DingKeys) > 0 {
+				//	dingKeys = dyAgoCfg.AppConfig.DingKeys
+				//}
+				//if len(dyAgoCfg.AppConfig.DingUsers) > 0 {
+				//	dingusers = dyAgoCfg.AppConfig.AppConfigMap[appid].DingUsers
+				//}
+				//for key, value := range dyAgoCfg.AppConfig.DingUserMap {
+				//	if userMap == nil {
+				//		userMap = map[string]string{}
+				//	}
+				//	userMap[key] = value
+				//}
+				//if dyAgoCfg.AppConfig.IsAtAll != 0 {
+				//	isAtallTmp = dyAgoCfg.AppConfig.IsAtAll
+				//}
+				InitDyAppConfigMap(dyAgoCfg.AppConfig,appid,isAtallTmp)
 			}
 			//uniq appid config
 			if dyAgoCfg.AppConfig.AppConfigMap != nil {
-				if _,ok := dyAgoCfg.AppConfig.AppConfigMap[appid];ok {
-					if len(dyAgoCfg.AppConfig.AppConfigMap[appid].DingKeys) > 0 {
-						dingKeys = dyAgoCfg.AppConfig.AppConfigMap[appid].DingKeys
-					}
-					if len(dyAgoCfg.AppConfig.AppConfigMap[appid].DingUsers) > 0{
-						dingusers = dyAgoCfg.AppConfig.AppConfigMap[appid].DingUsers
-					}
-					for key,value := range dyAgoCfg.AppConfig.AppConfigMap[appid].DingUserMap {
-						if userMap == nil {
-							userMap = map[string]string{}
-						}
-						userMap[key] = value
-					}
-					if dyAgoCfg.AppConfig.AppConfigMap[appid].IsAtAll != 0 {
-						isAtallTmp = dyAgoCfg.AppConfig.AppConfigMap[appid].IsAtAll
-					}
+				if _, ok := dyAgoCfg.AppConfig.AppConfigMap[appid]; ok {
+					//if len(dyAgoCfg.AppConfig.AppConfigMap[appid].DingKeys) > 0 {
+					//	dingKeys = dyAgoCfg.AppConfig.AppConfigMap[appid].DingKeys
+					//}
+					//if len(dyAgoCfg.AppConfig.AppConfigMap[appid].DingUsers) > 0 {
+					//	dingusers = dyAgoCfg.AppConfig.AppConfigMap[appid].DingUsers
+					//}
+					//for key, value := range dyAgoCfg.AppConfig.AppConfigMap[appid].DingUserMap {
+					//	if userMap == nil {
+					//		userMap = map[string]string{}
+					//	}
+					//	userMap[key] = value
+					//}
+					//if dyAgoCfg.AppConfig.AppConfigMap[appid].IsAtAll != 0 {
+					//	isAtallTmp = dyAgoCfg.AppConfig.AppConfigMap[appid].IsAtAll
+					//}
+					InitAppConfigMap(dyAgoCfg.AppConfig.AppConfigMap,appid,isAtallTmp)
 				}
 			}
 		}
 	}
-	for _,v := range dingKeys{
-		fmt.Println("dingKey为:"+v + "\n")
+	for _, v := range dingKeys {
+		fmt.Println("dingKey为:" + v + "\n")
 	}
-	for _,v := range dingusers{
-		fmt.Println("dingusers为:"+v + "\n")
+	for _, v := range dingusers {
+		fmt.Println("dingusers为:" + v + "\n")
 	}
-	for k,v := range userMap{
-		fmt.Println("userMapKey为:"+k+" "+"userMapValue为"+v+"\n")
+	for k, v := range userMap {
+		fmt.Println("userMapKey为:" + k + " " + "userMapValue为" + v + "\n")
 	}
-	fmt.Println("isAtAllTmp为:"+string(isAtallTmp))
+	fmt.Println("isAtAllTmp为:" + string(isAtallTmp))
 	if isAtallTmp == 1 {
 		isAtall = true
 	}
@@ -159,13 +162,13 @@ func (this *ccLogger) Warn(args ...interface{}) {
 	if this == nil || this.Runtime == nil {
 		return
 	}
-	if _,ok:=interface{}(args[0]).(string);ok {
-		dingkeys,dingusers,_,isatall := GetDingInfo(args[0].(string), "warn")
+	if _, ok := interface{}(args[0]).(string); ok {
+		dingkeys, dingusers, _, isatall := GetDingInfo(args[0].(string), "warn")
 
-		cnotify.SendText(dingkeys,fmt.Sprintf("%s",args),dingusers,isatall)
+		cnotify.SendText(dingkeys, fmt.Sprintf("%s", args), dingusers, isatall)
 		this.Runtime.Warn(args)
 	} else {
-		dingkeys,dingusers,usermap,isatall := GetDingInfo(args[1].(string), "warn")
+		dingkeys, dingusers, usermap, isatall := GetDingInfo(args[1].(string), "warn")
 		switch t := args[0].(type) {
 		case []string:
 			keyStringValues := []string{}
@@ -178,7 +181,7 @@ func (this *ccLogger) Warn(args ...interface{}) {
 		default:
 			fmt.Println("dingusers type error , need []string")
 		}
-		cnotify.SendText(dingkeys,fmt.Sprintf("%s",args[1:]),dingusers,isatall)
+		cnotify.SendText(dingkeys, fmt.Sprintf("%s", args[1:]), dingusers, isatall)
 		this.Runtime.Warn(args)
 	}
 }
@@ -187,17 +190,16 @@ func (this *ccLogger) Error(args ...interface{}) {
 	if this == nil || this.Runtime == nil {
 		return
 	}
-	dingkeys,dingusers,_,isatall := GetDingInfo(args[0].(string), "err")
-	cnotify.SendText(dingkeys,fmt.Sprintf("%s",args),dingusers,isatall)
+	dingkeys, dingusers, _, isatall := GetDingInfo(args[0].(string), "err")
+	cnotify.SendText(dingkeys, fmt.Sprintf("%s", args), dingusers, isatall)
 	this.Runtime.Error(args)
 }
-
 
 func (this *ccLogger) Infof(format string, args ...interface{}) {
 	if this == nil || this.Runtime == nil {
 		return
 	}
-	this.Runtime.Infof(format,args)
+	this.Runtime.Infof(format, args)
 }
 
 func (this *ccLogger) Warnf(format string, args ...interface{}) {
@@ -212,4 +214,40 @@ func (this *ccLogger) Errorf(format string, args ...interface{}) {
 		return
 	}
 	this.Runtime.Errorf(format, args)
+}
+func InitAppConfigMap(appConfigMap map[string]ConfigInfo, appid string, isAtAllTmp int) (dingKeys []string, dingusers []string, userMap map[string]string, isAtAll int) {
+	if len(appConfigMap[appid].DingKeys) > 0 {
+		dingKeys = AppConfiger.AppConfigMap[appid].DingKeys
+	}
+	if len(AppConfiger.AppConfigMap[appid].DingUsers) > 0 {
+		dingusers = AppConfiger.AppConfigMap[appid].DingUsers
+	}
+	for key, value := range AppConfiger.AppConfigMap[appid].DingUserMap {
+		if userMap == nil {
+			userMap = map[string]string{}
+		}
+		userMap[key] = value
+	}
+	if appConfigMap[appid].IsAtAll != 0 {
+		isAtAllTmp = appConfigMap[appid].IsAtAll
+	}
+	return
+}
+func InitDyAppConfigMap(dyAppConfigMap *AppCfg, appid string, isAtAllTmp int) (dingKeys []string, dingusers []string, userMap map[string]string, isAtAll int) {
+	if len(dyAppConfigMap.AppConfigMap[appid].DingKeys) > 0 {
+		dingKeys = AppConfiger.AppConfigMap[appid].DingKeys
+	}
+	if len(dyAppConfigMap.AppConfigMap[appid].DingUsers) > 0 {
+		dingusers = AppConfiger.AppConfigMap[appid].DingUsers
+	}
+	for key, value := range dyAppConfigMap.AppConfigMap[appid].DingUserMap {
+		if userMap == nil {
+			userMap = map[string]string{}
+		}
+		userMap[key] = value
+	}
+	if dyAppConfigMap.AppConfigMap[appid].IsAtAll != 0 {
+		isAtAllTmp = dyAppConfigMap.AppConfigMap[appid].IsAtAll
+	}
+	return
 }
