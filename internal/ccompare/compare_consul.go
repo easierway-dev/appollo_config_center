b92597963ede63c4d3ffb268e48bd69b37943543
jilong.jian <jilong.jian@mobvista.com>
2022-03-03 18:31:37 +0800

    fix86

--- internal/ccompare/compare_consul.go
+++ internal/ccompare/compare_consul.go
@@ -3,28 +3,31 @@ package ccompare
 import "fmt"
 
 const Application = "application"
+
 var notExistKey []string
 var notEqualKey []string
-func ApolloCompareWithConsul() error{
+
+func ApolloCompareWithConsul() error {
 	apolloKV, err := GetSingleNameSpaceInfo(Application)
-	if err != nil{
+	if err != nil {
 		return err
 	}
 	consulKV, err := GetConsulKV()
-	if err != nil{
+	if err != nil {
 		return err
 	}
 	for k, apolloValue := range apolloKV {
-		if consulValue,ok:= consulKV[k];ok{
-			if apolloValue != consulValue{
-				notEqualKey = append(notEqualKey,k)
-			}else {
+		if consulValue, ok := consulKV[k]; ok {
+			if apolloValue != consulValue {
+				notEqualKey = append(notEqualKey, k)
+			} else {
 				continue
 			}
+		} else {
+			notExistKey = append(notExistKey, k)
 		}
-		notExistKey = append(notExistKey,k)
 	}
-	fmt.Println("notEqualKey=",notEqualKey)
-	fmt.Println("notExistKey=",notExistKey)
+	fmt.Println("notEqualKey=", notEqualKey)
+	fmt.Println("notExistKey=", notExistKey)
 	return nil
 }
